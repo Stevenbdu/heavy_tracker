dd2e5666a42cf2e32571067f01a6b53b48565e46
Steven <steven.bedu@gmail.com>
2026-05-03 22:34:56 +0200

    test deploy

--- backend/app/api/hello/route.ts
+++ backend/app/api/hello/route.ts
@@ -1,33 +1,38 @@
+// import { NextResponse } from 'next/server';
+// import { PrismaClient } from '@prisma/client';
+// import { Pool } from 'pg';
+// import { PrismaPg } from '@prisma/adapter-pg';
+
+// // 1. On crée un pool de connexion avec l'URL de ton fichier .env
+// const connectionString = process.env.DATABASE_URL;
+// const pool = new Pool({ connectionString });
+
+// // 2. On passe le pool à l'adapter Prisma
+// const adapter = new PrismaPg(pool);
+
+// // 3. On initialise Prisma avec cet adapter
+// const prisma = new PrismaClient({ adapter });
+// console.log(prisma.program)
+
+// export async function GET() {
+//   try {
+//     const count = await prisma.program.count();
+    
+//     return NextResponse.json({ 
+//       status: "success",
+//       message: `Connexion DB réussie ! Il y a actuellement ${count} programme(s) enregistré(s).`
+//     });
+
+//   } catch (error) {
+//     console.error("Erreur de base de données:", error);
+//     return NextResponse.json(
+//       { status: "error", message: "Impossible de se connecter à la base de données." },
+//       { status: 500 }
+//     );
+//   }
+// }
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-
-// 1. On crée un pool de connexion avec l'URL de ton fichier .env
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-
-// 2. On passe le pool à l'adapter Prisma
-const adapter = new PrismaPg(pool);
-
-// 3. On initialise Prisma avec cet adapter
-const prisma = new PrismaClient({ adapter });
-console.log(prisma.program)
 
 export async function GET() {
-  try {
-    const count = await prisma.program.count();
-    
-    return NextResponse.json({ 
-      status: "success",
-      message: `Connexion DB réussie ! Il y a actuellement ${count} programme(s) enregistré(s).`
-    });
-
-  } catch (error) {
-    console.error("Erreur de base de données:", error);
-    return NextResponse.json(
-      { status: "error", message: "Impossible de se connecter à la base de données." },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({ message: "Le backend fonctionne !" });
 }
